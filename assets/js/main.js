ce50617ed75471fff33839d5d79ff274b3630113
mserafin20 <mmserafin20@gmail.com>
2023-04-25 19:35:09 -0400

    Added a form with seach button, browser responds

---
+++ assets/js/main.js
@@ -0,0 +1,8 @@
+// For Jquery $('button').on('click')
+var formEl = document.querySelector("#search-form");
+var resultsEl = document.querySelector("#results-container");
+var inputEl = document.querySelector('[name=search]');
+formEl.addEventListener('submit', function(event){
+    event.preventDefault();
+    resultsEl.textContent="Working? " + inputEl.value;
+});
